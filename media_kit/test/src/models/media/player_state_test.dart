4f0f72e5bf21349bc251bf52761628faeae3f233
My-Responsitories <107370289+My-Responsitories@users.noreply.github.com>
2026-05-20 12:51:11 +0800

    fix: ulong cast

--- media_kit/test/src/models/media/player_state_test.dart
+++ media_kit/test/src/models/media/player_state_test.dart
@@ -4,12 +4,12 @@
 /// All rights reserved.
 /// Use of this source code is governed by MIT license that can be found in the LICENSE file.
 
+import 'package:media_kit/src/models/playable.dart';
 import 'package:media_kit/src/models/subtitle.dart';
 import 'package:test/test.dart';
 import 'package:collection/collection.dart';
 
 import 'package:media_kit/src/models/track.dart';
-import 'package:media_kit/src/models/playlist.dart';
 import 'package:media_kit/src/models/player_state.dart';
 import 'package:media_kit/src/models/playlist_mode.dart';
 
